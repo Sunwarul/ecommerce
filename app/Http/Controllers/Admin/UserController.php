117192847c34809f041d3330ef1fcf323973644e
jahid-facts <jahid.facts@gmail.com>
2025-11-04 22:34:46 +0600

    update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -39,7 +39,6 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-
         $this->ensureModelClass();
         $validatedData = app($this->storeRequestClass)->validated();
         if ($request->file('photo')) {
