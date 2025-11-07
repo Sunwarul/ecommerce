a468d25ebea4df8ba99d3ca2f65c382d2025c0f5
jahid-facts <jahid.facts@gmail.com>
2025-11-07 14:13:55 +0600

    feat: currency

---
+++ app/Models/Currency.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Currency extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'short_code',
+        'date',
+        'symbol',
+        'exchange_rate',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'integer',
+            'date' => 'date',
+            'exchange_rate' => 'decimal',
+            'status' => 'boolean',
+        ];
+    }
+}
