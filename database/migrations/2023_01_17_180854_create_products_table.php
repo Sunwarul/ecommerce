c9f44ee422c2185c1a17772696c1f56eef17a2f7
Sunwarul Islam <sanoarulslm@gmail.com>
2025-11-20 00:12:23 +0600

    fix: fix for windows and migrate fresh

--- database/migrations/2023_01_17_180854_create_products_table.php
+++ database/migrations/2023_01_17_180854_create_products_table.php
@@ -19,8 +19,6 @@ return new class extends Migration
             $table->foreignId('tax_id')->nullable()->constrained('taxes')->onDelete('CASCADE');
             $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('CASCADE');
             $table->foreignId('unit_id')->nullable()->constrained('units')->nullOnDelete();
-            $table->foreignId('tax_id')->nullable()->constrained('taxes')->onDelete('CASCADE');
-            $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('CASCADE');
             $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('CASCADE');
 
             $table->string('name');
