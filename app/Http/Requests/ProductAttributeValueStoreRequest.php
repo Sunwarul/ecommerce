1aaa9676335bf3d29aa2e0651f84e31ff6f09b9e
Sunwarul Islam <sanoarulslm@gmail.com>
2026-02-04 00:20:19 +0600

    fix: invoice fixed with logos, and other modifications

---
+++ app/Http/Requests/ProductAttributeValueStoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductAttributeValueStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'attribute_id' => ['required', 'integer'],
+            // 'attribute_id' => ['required', 'integer', 'exists:product_attributes.id,id'],
+            'value' => ['required', 'string'],
+            'display_value' => ['nullable', 'string'],
+            'color_code' => ['nullable', 'string'],
+            'deleted_at' => ['nullable'],
+        ];
+    }
+}
