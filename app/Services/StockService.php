fa933609529530facebf582a19cdd0e26ebfb6f2
Sunwarul Islam <sanoarulslm@gmail.com>
2026-03-02 23:59:52 +0600

    feat: allow sale from single warehouse from multiple branch.

--- app/Services/StockService.php
+++ app/Services/StockService.php
@@ -39,7 +39,7 @@ class StockService
                 'product_id' => $data['product_id'],
                 'variation_id' => $data['variation_id'] ?? null,
                 'warehouse_id' => $data['from_warehouse_id'],
-                'branch_id' => $data['branch_id'],
+                // 'branch_id' => $data['branch_id'],
             ])->lockForUpdate()->first();
 
             if (!$stock || $stock->quantity < $data['quantity']) {
